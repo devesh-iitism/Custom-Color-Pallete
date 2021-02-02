439eb796e30f747429a560941f46d1658f567a6f
root <dcdev001@gmail.com>
2021-02-02 13:55:08 +0000

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {Router, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
@@ -6,9 +7,14 @@ import { generatePalette } from "./colorHelpers";
 class App extends Component {
 	render() {
 		return (
-			<div>
-				<Palette palette={generatePalette(seedColors[4])} />
-			</div>
+			<Switch>
+				<Router exact path="/" />
+				<Router exact path="/palette/:id" />
+			</Switch>
+			
+			// <div className="App">
+			// 	<Palette palette={generatePalette(seedColors[4])} />
+			// </div>
 		)
 	}
 }
---
+++ src/feedback.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class feedback extends Component {
+	render() {
+		return (
+			<div>
+				<h1 className="container">Hii Hello!</h1>
+			</div>
+		)
+	}
+}
+
+export default feedback;
