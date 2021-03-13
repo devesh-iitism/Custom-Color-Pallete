985a39ac45e8ff31c8bb01767f2cbf3a7213a7b5
root <dcdev001@gmail.com>
2021-03-13 17:13:37 +0000

    Saving New Palette

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -87,6 +87,7 @@ class NewPaletteForm extends Component {
 		this.updateCurrentColor = this.updateCurrentColor.bind(this);
 		this.addNewColor = this.addNewColor.bind(this);
 		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 	
 	componentDidMount() {
@@ -127,6 +128,17 @@ class NewPaletteForm extends Component {
 	  this.setState({newName: evt.target.value})
   }
 
+  handleSubmit() {
+	  let newName = "New Test Palette";
+	  const newPalette = {
+		  paletteName: newName,
+		  id: newName.toLowerCase().replace(/ /g, "-"),
+		  colors: this.state.colors
+	  }
+	  this.props.savePalette(newPalette);
+	  this.props.history.push("/");
+  }
+
   render() {
     const { classes } = this.props;
     const { open } = this.state;
@@ -135,6 +147,7 @@ class NewPaletteForm extends Component {
       <div className={classes.root}>
         <CssBaseline />
         <AppBar
+		  color="primary"
           position='fixed'
           className={classNames(classes.appBar, {
             [classes.appBarShift]: open
@@ -152,6 +165,7 @@ class NewPaletteForm extends Component {
             <Typography variant='h6' color='inherit' noWrap>
               Persistent drawer
             </Typography>
+			<Button variant="contained" color="secondary" onClick={this.handleSubmit}>Save Palette</Button>
           </Toolbar>
         </AppBar>
         <Drawer
